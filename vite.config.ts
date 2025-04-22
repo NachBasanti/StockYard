54557cdaabcb367fc10ae196aed5ca9abc0e7803
Pratyush Mishra <pratyushm704@gmail.com>
2025-04-22 14:11:35 +0530

    made it vercel compatible

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(),    tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      external: ['xlsx'] // ✅ Tell Rollup to ignore bundling xlsx
+    }
+  },
+  optimizeDeps: {
+    exclude: ['xlsx'] // ✅ Skip trying to pre-bundle xlsx
+  }
   
 })
